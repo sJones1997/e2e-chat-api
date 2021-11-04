317f8c25eff3179f48cbcf9ca4c9d100892c4c12
Sean <sean.g.jones97@gmail.com>
2021-11-04 21:30:36 +0000

    adding in final check

--- socket/messages.js
+++ socket/messages.js
@@ -2,18 +2,22 @@
 module.exports = (io, socket) => {
     socket.on("send-message", async (messageObj, cb) => {
         if(Object.entries(messageObj).length){
-            const userInRoom = await socket.userRoomService.isUserInRoom(socket.id, messageObj.roomId);
-            if(userInRoom.status){
-                messageObj.userId = socket.id;
-                const messageSaved = await socket.messageService.saveMessage(messageObj);
-                if(messageSaved.status){
-                    // socket.to(room).emit('receive-message', messageObj.message);
-                    cb(true, 'Message sent!')
+            if(messageObj.message.length){
+                const userInRoom = await socket.userRoomService.isUserInRoom(socket.id, messageObj.roomId);
+                if(userInRoom.status){
+                    messageObj.userId = socket.id;
+                    const messageSaved = await socket.messageService.saveMessage(messageObj);
+                    if(messageSaved.status){
+                        // socket.to(room).emit('receive-message', messageObj.message);
+                        cb(true, 'Message sent!')
+                    } else {
+                        cb(false, 'Something went wrong');                     
+                    }
                 } else {
-                    cb(false, 'Something went wrong');                     
-                }
+                    cb(false, "You are not in this room!");
+                }                
             } else {
-                cb(false, "You are not in this room!");
+                cb(false)
             }
         } else {
             cb(false, 'Something went wrong');            
